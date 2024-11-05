42fb2826a439a74a2bcda2674e00f7dbfccfc7e6
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-05 16:52:34 +0530

    mohabbat

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,7 @@
-
-
-function App() {
-  return (
-    <h1>classic react app</h1>
-  );
+import Youtube from "./Youtube"
+function App(){
+    return(
+        <><h1>classic react app</h1><Youtube /></>
+    );
 }
-
 export default App;
