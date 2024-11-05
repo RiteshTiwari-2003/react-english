09befca9899b140ce93b8e48255d676309738221
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-06 00:31:57 +0530

    i think this is commit

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+const reactElement={
+    type:"a",
+    props:{
+        href:"https://google.com",
+        target:'_blank'
+    },
+    children:"Click me to vigit Google"
+}
+function MyApp(){
+    return(
+        <div>Custom react app</div>
+    )
+}
+const AnotherElement=(
+    <a href="https://www.google.com" target="_blank">Visit Google</a>
+)
+const AReactElement=React.createElement(
+    'a',
+    {href:'https://www.google.com', target:'_blank'},
+    'click to visit google'
+)
+
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    //AnotherElement
+    AReactElement
   
 )
