19dc21dd2c420498ff656c722596e09597b4a907
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-06 23:53:36 +0530

    this is my eleventh commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -6,12 +6,22 @@ import Cards from "./components/Cards"
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    name:"ritesh",
+    age:25,
+    address:{
+      city:"Banglore",
+      state:"Karnataka",
+      country:"India"
+    }
+  }
+  let newArray=[1,2,3,4]
 
   return (
     <>
       <h1 className="text-3xl bg-green-500 p-3 rounded md">vite with tailwind</h1>
-      <Cards username="ritesh"/>
-      <Cards/>
+      <Cards username="ritesh" post={newArray}/>
+      <Cards username="JSON" post="Staff Engineer"/>
       <Cards/>
 
     </>
