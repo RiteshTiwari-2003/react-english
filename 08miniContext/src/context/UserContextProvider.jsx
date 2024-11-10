529576dc06d2dbae00f67c1a986f860cad856734
Ritesh <riteshtiwari3112003@gmail.com>
2024-11-10 15:57:43 +0530

    this is my twenty two commit

---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+//this is jsx because this wrapper 
+import React,{useState} from "react"
+import UserContext from "./UserContext"
+const UserContextProvider=({children})=>{
+    const [user,setUser]=useState(null)
+    return (
+        <UserContext.Provider value={{user,setUser}}>{children}</UserContext.Provider>
+
+    )
+}
+export default UserContextProvider
